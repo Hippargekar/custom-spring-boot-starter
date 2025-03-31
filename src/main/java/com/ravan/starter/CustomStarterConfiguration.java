841409b5a3ac4224350bad31675327b1b6863030
Pritam Hippargekar <pritam.hippargekar@viraltech.in>
2025-03-31 09:20:08 +0530

    removed import

--- src/main/java/com/ravan/starter/CustomStarterConfiguration.java
+++ src/main/java/com/ravan/starter/CustomStarterConfiguration.java
@@ -1,6 +1,5 @@
 package com.ravan.starter;
 
-import com.ravan.starter.enums.SmsTypeEnum;
 import com.ravan.starter.exception.InvalidSmsTypeException;
 import com.ravan.starter.properties.ExternalSmsProperties;
 import com.ravan.starter.service.SmsService;
@@ -12,7 +11,6 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Map;
